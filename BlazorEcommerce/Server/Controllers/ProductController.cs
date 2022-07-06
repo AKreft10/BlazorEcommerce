154769c19f180bc78e4c94c93340eda519b27d67
Arek Kreft <arek.kreft@poczta.onet.pl>
2022-07-06 17:02:50 +0200

    App walking skeleton

---
+++ BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorEcommerce.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ProductController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ProductController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProduct()
+        {
+            var products = await _context
+                .Products
+                .ToListAsync();
+
+            return Ok(products); 
+        }
+    }
+}
